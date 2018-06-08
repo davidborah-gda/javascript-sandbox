54e340411b8ab3682d2b1a0bc00413330649f480
David Borah <dborah87@gmail.com>
2018-06-07 20:11:34 -0400

    created some strings and learned about string methods, accessing individual characters within a string, how to capitalize a string using methods and character index

---
+++ strings/beginning.js
@@ -0,0 +1,11 @@
+ // We can create strings with literals!
+const good = 'sleep';
+const food = 'cheese';
+const dreamJob = 'hotdog vendor'; //This is a camelCase variable
+
+//capitalize dreamJob, but we have to create a new string becuase
+//we can't make cahnges to the old one
+const capDreamJob = dreamJob[0].toUpperCase() + dreamJob.slice(1);
+console.log("capDreamJob: ", capDreamJob);
+
+
