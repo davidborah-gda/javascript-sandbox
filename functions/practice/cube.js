bffdfab8294e9bd52e26b1446a38edec40b4b9fb
David Borah <dborah87@gmail.com>
2018-06-11 20:58:58 -0400

    Filling out the @parameter list for the square and cube functions

---
+++ functions/practice/cube.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {number} n some number
+ * @return {number} the cube or (n ^3) or (n * n * n)
+ */
+function cube(n){
+    return n * n * n;
+}
+
+
+console.log("cube(3) should be 27", cube(3));
+console.log("cube(4) should be 64", cube(3));
+console.log("cube(5) should be 128", cube(3));
---
+++ functions/practice/square.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {number} n 
+ * @return {number} the square or (n ^ 2) or (n * n)
+ */
+function square(n){
+    return n * n;
+}
+
+
+console.log("square(2) should be 4", cube(2));
+console.log("square(3) should be 9", cube(3));
+console.log("square(4) should be 16", cube(4));
