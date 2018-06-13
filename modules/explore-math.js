66d2558d3bf36effdf828210c2562c455b89432e
David Borah <dborah87@gmail.com>
2018-06-12 20:13:18 -0400

    explored the use of the created math module

---
+++ modules/explore-math.js
@@ -0,0 +1,14 @@
+const math = require('./math.js');
+
+console.log("The module contains", math);
+
+//a module can contain functions (also known as behaviors)
+
+const sum = math.add(1, 2);
+console.log("The sum is ", sum);
+const difference = math.sub(1, 2);
+console.log("The difference is ", difference);
+
+//a module can also contain special values
+console.log("the value of pi is ", math.PI);
+console.log("teh value of e is ", math.E);
